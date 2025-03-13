3d26d72fa43374cdee974add4ec3239ebb1bc036
nhattpam <nhat2up@gmail.com>
2025-03-13 08:54:26 +0700

    reverse string

---
+++ AlgorithmBasic/ReverseString/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class ReverseString
+{
+    public static string Reverse(string str)
+    {
+        string reversed = ""; // Chuỗi kết quả
+
+        // Duyệt chuỗi từ ký tự cuối đến đầu
+        for (int i = str.Length - 1; i >= 0; i--)
+        {
+            reversed += str[i]; // Nối ký tự vào chuỗi reversed
+        }
+
+        return reversed; // Trả về chuỗi đã đảo ngược
+    }
+
+    static void Main()
+    {
+        Console.WriteLine(Reverse("automation")); // Output: "noitamotua"
+    }
+}
